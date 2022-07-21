2d1bbee9744eea84b28ece9e09227506c16dad61
David LeBlanc <email@david-leblanc.com>
2022-07-21 19:45:21 -0400

    Prettier Cleanup

--- scripts/start.mjs
+++ scripts/start.mjs
@@ -7,18 +7,18 @@ import { paths } from './utils.mjs'
 import { buildWebpackConfig } from '../config/webpack.devel.config.mjs'
 
 console.clear()
-console.log("Begin build...")
+console.log('Begin build...')
 
-const spinner = ora("Building for development...\n")
+const spinner = ora('Building for development...\n')
 spinner.start()
 
 const webpackConfig = buildWebpackConfig(paths)
 const { devServer: devServerConfig } = webpackConfig
 
 rm(paths.build, (rmError) => {
-   if (rmError) {
-     throw rmError
-   }
+  if (rmError) {
+    throw rmError
+  }
 
   const compiler = webpack(webpackConfig, (webpackError, stats) => {
     spinner.stop()
@@ -27,19 +27,19 @@ rm(paths.build, (rmError) => {
       throw webpackError
     }
 
-    console.log("Build complete.\n")
+    console.log('Build complete.\n')
 
-    process.stdout.write(`${
-        stats.toString({
-          colors: true,
-          modules: false,
-          children: false,
-          chunks: false,
-          chunkModules: false
-        })
-      }\n\n`)
+    process.stdout.write(
+      `${stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false,
+      })}\n\n`
+    )
 
     const server = new webpackDevServer(devServerConfig, compiler)
-    server.start();
+    server.start()
   })
 })
